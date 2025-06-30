862329e6c2de7cb29f74e967be515f4c0f64af3d
Phuc-Nguyenn <nlgphuc23@gmail.com>
2025-06-30 10:09:36 +0930

    removed object filers

--- run.sh
+++ run.sh
@@ -3,16 +3,16 @@ vertex_glsl_path="./src/shaders/Vertex.glsl"
 fragment_glsl_path="./src/shaders/Fragment.glsl"
 skybox_directory_path="./Textures/DaylightBox"
 object_paths="
-./Objects/sponza-palace.off
-./Objects/dragon.off
-./Objects/sharpsphere.off"
+../Objects/sponza-palace.off
+../Objects/dragon.off
+../Objects/sharpsphere.off"
 
-# ./Objects/sponza-palace.off
-# ./Objects/dragon.off
-# ./Objects/sharpsphere.off
-# ./Objects/livingroom.off
-# ./Objects/teapot1.txt
-# ./Objects/teapot2.txt
+# ../Objects/sponza-palace.off
+# ../Objects/dragon.off
+# ../Objects/sharpsphere.off
+# ../Objects/livingroom.off
+# ../Objects/teapot1.txt
+# ../Objects/teapot2.txt
 
 # Navigate to the build directory
 ./build/ray_tracer $vertex_glsl_path $fragment_glsl_path $skybox_directory_path $object_paths
